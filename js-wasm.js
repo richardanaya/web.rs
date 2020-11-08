2e15c35ee97abb772d2061c91fd405697824b26b
Richard <Richard>
2020-11-07 17:21:30 -0800

    add ability to create environment

--- js-wasm.js
+++ js-wasm.js
@@ -1,77 +1,76 @@
-async function load_and_run_wasm(wasmURL) {
-  let context = {
-    functions: [],
-    free_locations: [],
-    objects: [
-      undefined,
-      null,
-      self,
-      typeof document != "undefined" ? document : null,
-      typeof document != "undefined" ? document.body : null
-    ],
-    utf8dec: new TextDecoder("utf-8"),
-    utf8enc: new TextEncoder("utf-8"),
-    createCallback: function(cb){
-      let fnHandleCallback = this.module.instance.exports.handle_callback;
-      return function(){
-        const args = arguments;
-        fnHandleCallback(cb,args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9]);
+window.JsWasm = {
+  createEnvironment() {
+    let context = {
+      functions: [],
+      free_locations: [],
+      objects: [
+        undefined,
+        null,
+        self,
+        typeof document != "undefined" ? document : null,
+        typeof document != "undefined" ? document.body : null
+      ],
+      utf8dec: new TextDecoder("utf-8"),
+      utf8enc: new TextEncoder("utf-8"),
+      createCallback: function (cb) {
+        let fnHandleCallback = this.module.instance.exports.handle_callback;
+        return function () {
+          const args = arguments;
+          fnHandleCallback(cb, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
+        }
+      },
+      readCStringFromMemory: function (start) {
+        const data = new Uint8Array(this.module.instance.exports.memory.buffer);
+        const str = [];
+        let i = start;
+        while (data[i] !== 0) {
+          str.push(data[i]);
+          i++;
+        }
+        return this.utf8dec.decode(new Uint8Array(str));
+      },
+      writeCStringToMemory(str, start) {
+        let bytes = this.utf8enc.encode(str + String.fromCharCode(0));
+        let len = bytes.length;
+        const memory = new Uint8Array(this.module.instance.exports.memory.buffer);
+        memory.set(bytes, start);
+      },
+      readUtf8FromMemory: function (start, len) {
+        let memory = new Uint8Array(this.module.instance.exports.memory.buffer);
+        let text = this.utf8dec.decode(memory.subarray(start, start + len));
+        return text;
+      },
+      readUint8ArrayFromMemory(start) {
+        const data32 = new Uint32Array(this.module.instance.exports.memory.buffer);
+        const ptr = data32[start / 4];
+        const length = data32[ptr / 4];
+        let b = mem.slice(ptr + 4, ptr + 4 + length);
+        return new Uint8Array(b);
+      },
+      writeUtf8ToMemory: function (start, str) {
+        let bytes = utf8enc.encode(str);
+        let len = bytes.length;
+        const memory = new Uint8Array(this.module.instance.exports.memory.buffer);
+        memory.set(bytes, start);
+      },
+      storeObject: function (obj) {
+        let handle = this.objects.length;
+        if (this.free_locations.length > 0) {
+          handle = this.free_locations.pop();
+        }
+        this.objects.push(obj);
+        return handle;
+      },
+      getObject: function (handle) {
+        return this.objects[handle];
+      },
+      releaseObject: function (handle) {
+        this.objects[handle] = null;
+        this.free_locations.push(handle);
       }
-    },
-    readCStringFromMemory: function (start) {
-      const data = new Uint8Array(this.module.instance.exports.memory.buffer);
-      const str = [];
-      let i = start;
-      while (data[i] !== 0) {
-        str.push(data[i]);
-        i++;
-      }
-      return this.utf8dec.decode(new Uint8Array(str));
-    },
-    writeCStringToMemory(str, start) {
-      let bytes = this.utf8enc.encode(str + String.fromCharCode(0));
-      let len = bytes.length;
-      const memory = new Uint8Array(this.module.instance.exports.memory.buffer);
-      memory.set(bytes, start);
-    },
-    readUtf8FromMemory: function (start, len) {
-      let memory = new Uint8Array(this.module.instance.exports.memory.buffer);
-      let text = this.utf8dec.decode(memory.subarray(start, start + len));
-      return text;
-    },
-    readUint8ArrayFromMemory(start) {
-      const data32 = new Uint32Array(this.module.instance.exports.memory.buffer);
-      const ptr = data32[start / 4];
-      const length = data32[ptr / 4];
-      let b = mem.slice(ptr + 4, ptr + 4 + length);
-      return new Uint8Array(b);
-    },
-    writeUtf8ToMemory: function (start, str) {
-      let bytes = utf8enc.encode(str);
-      let len = bytes.length;
-      const memory = new Uint8Array(this.module.instance.exports.memory.buffer);
-      memory.set(bytes, start);
-    },
-    storeObject: function (obj) {
-      let handle = this.objects.length;
-      if (this.free_locations.length > 0) {
-        handle = this.free_locations.pop();
-      }
-      this.objects.push(obj);
-      return handle;
-    },
-    getObject: function (handle) {
-      return this.objects[handle];
-    },
-    releaseObject: function (handle) {
-      this.objects[handle] = null;
-      this.free_locations.push(handle);
-    }
-  };
-  let response = await fetch(wasmURL);
-  let bytes = await response.arrayBuffer();
-  let module = await WebAssembly.instantiate(bytes, {
-    env: {
+    };
+    return {
+      context,
       js_register_function(start, len) {
         let functionBody = context.readUtf8FromMemory(start, len);
         let id = context.functions.length;
@@ -94,17 +93,26 @@ async function load_and_run_wasm(wasmURL) {
         );
       }
     }
-  });
-  context.module = module;
-  let num = module.instance.exports.main();
+  },
+
+  async load_and_run_wasm(wasmURL) {
+    const env = JsWasm.createEnvironment();
+    const response = await fetch(wasmURL);
+    const bytes = await response.arrayBuffer();
+    const module = await WebAssembly.instantiate(bytes, {
+      env
+    });
+    env.context.module = module;
+    module.instance.exports.main();
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  let wasmScripts = document.querySelectorAll("script[type='application/wasm']");
+  const wasmScripts = document.querySelectorAll("script[type='application/wasm']");
   for (let i = 0; i < wasmScripts.length; i++) {
-    let src = wasmScripts[i].src;
+    const src = wasmScripts[i].src;
     if (src) {
-      load_and_run_wasm(src);
+      JsWasm.load_and_run_wasm(src);
     } else {
       console.error("Script tag must have 'src' property.");
     }
