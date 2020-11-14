f33fb2e5b15fe3a3075e595e046f8ac3b188198c
Richard <Richard>
2020-11-14 08:56:12 -0800

    using lazy

--- crates/web_dom/src/lib.rs
+++ crates/web_dom/src/lib.rs
@@ -3,17 +3,16 @@ use callback::*;
 use js::*;
 
 pub fn get_element_by_id(id: &str) -> JSObject {
-    static FN: once_cell::sync::OnceCell<JSFunction> = once_cell::sync::OnceCell::new();
-    FN.get_or_init(|| {
+    lazy_static::lazy_static! {
+        static ref FN: JSFunction= {
         register_function(
             "function(strPtr,strLen){
                     let el = document.getElementById(this.readUtf8FromMemory(strPtr,strLen)); 
                     return this.storeObject(el);
             }",
         )
-    })
-    .invoke_2(id.as_ptr() as u32, id.len() as u32)
-    .into()
+    };};
+    FN.invoke_2(id.as_ptr() as u32, id.len() as u32).into()
 }
 
 pub fn add_event_listener(
@@ -22,8 +21,8 @@ pub fn add_event_listener(
     handler: impl FnMut(f64) -> () + Send + 'static,
 ) {
     let cb = create_callback_1(handler);
-    static FN: once_cell::sync::OnceCell<JSFunction> = once_cell::sync::OnceCell::new();
-    FN.get_or_init(|| {
+    lazy_static::lazy_static! {
+        static ref FN: JSFunction= {
         register_function(
             "function(el,strPtr,strLen,callback){
                     el = this.getObject(el);
@@ -31,8 +30,8 @@ pub fn add_event_listener(
                     el.addEventListener(event,this.createCallback(callback));
             }",
         )
-    })
-    .invoke_4(dom.into(), event.as_ptr() as u32, event.len() as u32, cb);
+    };};
+    FN.invoke_4(dom.into(), event.as_ptr() as u32, event.len() as u32, cb);
 }
 
 pub struct KeyDownEvent {
@@ -45,15 +44,15 @@ impl KeyDownEvent {
     }
 
     pub fn key_code(&self) -> u32 {
-        static FN: once_cell::sync::OnceCell<JSFunction> = once_cell::sync::OnceCell::new();
-        FN.get_or_init(|| {
+        lazy_static::lazy_static! {
+            static ref FN: JSFunction= {
             register_function(
                 "function(ev){
                         ev = this.getObject(ev);
                         return ev.keyCode;
                 }",
             )
-        })
-        .invoke_1(&self.handle) as u32
+        };};
+        FN.invoke_1(&self.handle) as u32
     }
 }
