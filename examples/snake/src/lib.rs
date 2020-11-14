9bae762cca51e18b42f0add4f40b6a56a2a8bf7d
Richard <Richard>
2020-11-14 09:44:28 -0800

    ecs stnake

--- examples/snake/src/lib.rs
+++ examples/snake/src/lib.rs
@@ -1,10 +1,13 @@
+use hecs::*;
 use web::*;
 
 struct Game {
+    time: u32,
     ctx: CanvasContext,
     width: u32,
     height: u32,
     direction: Direction,
+    world: World,
 }
 
 enum Direction {
@@ -14,6 +17,13 @@ enum Direction {
     Down,
 }
 
+struct SnakeHead;
+struct SnakeBody;
+struct Food;
+struct Position(f64, f64);
+struct Color(String);
+struct Size(f64);
+
 fn game() -> MutexGuard<'static, Game> {
     lazy_static::lazy_static! {
         static ref SINGLETON: Mutex<Game> = {
@@ -21,15 +31,37 @@ fn game() -> MutexGuard<'static, Game> {
             let width: f64 = get_property(&screen, "width");
             let height: f64 = get_property(&screen, "height");
             let ctx = CanvasContext::from_canvas_element(&screen);
+            let mut world = World::new();
+            world.spawn(
+                (SnakeHead,Color("green".to_string()),Position(0.0,0.0),Size(10.0))
+            );
+            world.spawn(
+                (Food,Color("red".to_string()),Position(50.0,50.0),Size(10.0))
+            );
             Mutex::new(Game {
+                time: 0,
                 ctx,
                 width: width as u32,
                 height: height as u32,
                 direction: Direction::Down,
+                world,
             })
         };
     }
     SINGLETON.lock()
+}
+
+fn move_snake(game: &Game) {
+    for (_id, (_, pos)) in &mut game.world.query::<(&SnakeHead, &mut Position)>() {
+        pos.0 += 10.0;
+    }
+}
+
+fn render_system(game: &Game) {
+    for (_id, (pos, color, size)) in &mut game.world.query::<(&Position, &Color, &Size)>() {
+        game.ctx.set_fill_color(&color.0);
+        game.ctx.fill_rect(pos.0, pos.1, size.0, size.0);
+    }
 }
 
 #[no_mangle]
@@ -50,12 +82,13 @@ pub fn main() {
 
     game().ctx.set_fill_color("red");
 
-    request_animation_loop(|_delta| {
-        let game = game();
-        for x in 0..game.width / 10 {
-            for y in 0..game.height / 10 {
-                game.ctx.fill_rect(x * 10, y * 10, 10, 10);
-            }
+    request_animation_loop(|delta| {
+        let mut game = game();
+        game.time += delta as u32;
+        if game.time > 1000 {
+            game.time %= 1000;
+            move_snake(&game)
         }
+        render_system(&game)
     });
 }
