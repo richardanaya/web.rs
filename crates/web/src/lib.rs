62ee32b84218553b482612ef960c71c5569da2ff
Richard <Richard>
2020-11-14 09:03:17 -0800

    lazy static

--- crates/web/src/lib.rs
+++ crates/web/src/lib.rs
@@ -1,10 +1,9 @@
 #![no_std]
+pub use js::*;
+pub use spin::{Mutex, MutexGuard};
+pub use web_canvas::*;
 pub use web_common::*;
-pub use web_dom::*;
 pub use web_console::*;
+pub use web_dom::*;
 pub use web_random::*;
 pub use web_timer::*;
-pub use web_canvas::*;
-pub use js::*;
-pub use once_cell::sync::OnceCell;
-pub use spin::{Mutex,MutexGuard};
