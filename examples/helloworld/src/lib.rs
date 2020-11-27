2640de9fab37c5fb020976618066a021a11e1319
Richard <Richard>
2020-11-27 11:40:19 -0800

    fix

--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -1,8 +1,10 @@
-use js::*;
+use web::*;
 
 #[no_mangle]
 pub fn main() {
-    let fn_log = js!("hey");
+    let fn_log = js!("function(strPtr,strLen){
+        console.log(this.readUtf8FromMemory(strPtr,strLen)); 
+    }");
     let msg = "Hello World!";
     fn_log.invoke_2(msg.as_ptr() as u32, msg.len() as u32);
 }
