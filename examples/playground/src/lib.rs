c2dc89c6a7c343b7949a88c2b96e8358168a9ecf
Richard <Richard>
2020-11-27 11:20:23 -0800

    fixes

--- examples/playground/src/lib.rs
+++ examples/playground/src/lib.rs
@@ -2,8 +2,6 @@ use web::*;
 
 #[no_mangle]
 pub fn main() {
-    let e = get_element_by_id("foo");
-    let s = attach_shadow(&e,true);
-    let t = get_attribute(&e,"bar");
-    set_inner_html(&s,&t.unwrap());
+    log("Hey");
 }
+
