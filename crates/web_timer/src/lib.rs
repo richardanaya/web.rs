8648ad49ebe6c5fa7564f4d5677b64bd50ca52dc
Richard <Richard>
2020-11-27 10:24:24 -0800

    use macro

--- crates/web_timer/src/lib.rs
+++ crates/web_timer/src/lib.rs
@@ -10,32 +10,19 @@ pub fn set_timeout(
     milliseconds: impl Into<f64>,
 ) -> (Handle, JSFunction) {
     let cb = create_callback_0(callback);
-    let fn = {
-        lazy_static::lazy_static! {
-        static ref FN: JSFunction= {
-            register_function(
-                "function(handler,time){
-                    window.setTimeout(this.createCallback(handler),time);
-                }",
-            )
-        };};
-        &FN
-    };
-    let handle = fn.invoke_2(cb, milliseconds);
+    let handle = js!("function(handler,time){
+        window.setTimeout(this.createCallback(handler),time);
+    }")
+    .invoke_2(cb, milliseconds);
     (handle, cb.into())
 }
 
 pub fn sleep(milliseconds: impl Into<f64>) -> impl Future {
     let (future, cb) = create_callback_future_0();
-    lazy_static::lazy_static! {
-        static ref FN: JSFunction= {
-        register_function(
-            "function(handler,time){
-                window.setTimeout(this.createCallback(handler),time);
-            }",
-        )
-    };};
-    FN.invoke_2(cb, milliseconds);
+    js!("function(handler,time){
+        window.setTimeout(this.createCallback(handler),time);
+    }")
+    .invoke_2(cb, milliseconds);
     future
 }
 
@@ -44,75 +31,50 @@ pub fn set_interval(
     milliseconds: impl Into<f64>,
 ) -> (Handle, JSFunction) {
     let cb = create_callback_0(callback);
-    lazy_static::lazy_static! {
-    static ref FN: JSFunction= {
-        register_function(
-            "function(handler,time){
-                window.setInterval(this.createCallback(handler),time);
-            }",
-        )
-    };};
-    let handle = FN.invoke_2(cb, milliseconds);
+    let handle = js!("function(handler,time){
+        window.setInterval(this.createCallback(handler),time);
+    }")
+    .invoke_2(cb, milliseconds);
     (handle, cb.into())
 }
 
 pub fn request_animation_frame(callback: impl FnMut() -> () + Send + 'static) -> JSFunction {
     let cb = create_callback_0(callback);
-    lazy_static::lazy_static! {
-        static ref FN: JSFunction= {
-        register_function(
-            "function(handler){
-                window.requestAnimationFrame(this.createCallback(handler));
-            }",
-        )
-    };};
-    FN.invoke_1(cb);
+    js!("function(handler){
+        window.requestAnimationFrame(this.createCallback(handler));
+    }")
+    .invoke_1(cb);
     cb.into()
 }
 
 pub fn request_animation_loop(callback: impl FnMut(f64) -> () + Send + 'static) -> JSFunction {
     let cb = create_callback_1(callback);
-    lazy_static::lazy_static! {
-        static ref FN: JSFunction= {
-        register_function(
-            "function(cb){
-                cb = this.createCallback(cb);
-                let time = Date.now();
-                function run(){
-                    let new_time = Date.now();
-                    let delta = new_time-time;
-                    time = new_time;
-                    window.requestAnimationFrame(run);
-                    cb(delta);
-                }
-                window.requestAnimationFrame(run);
-            }",
-        )
-    };};
-    FN.invoke_1(cb);
+    js!("function(cb){
+        cb = this.createCallback(cb);
+        let time = Date.now();
+        function run(){
+            let new_time = Date.now();
+            let delta = new_time-time;
+            time = new_time;
+            window.requestAnimationFrame(run);
+            cb(delta);
+        }
+        window.requestAnimationFrame(run);
+    }")
+    .invoke_1(cb);
     cb.into()
 }
 
 pub fn clear_timeout(handle: Handle) {
-    lazy_static::lazy_static! {
-        static ref FN: JSFunction= {
-        register_function(
-            "function(handle){
-                window.clearTimeout(handle);
-            }",
-        )
-    };};
-    FN.invoke_1(handle);
+    js!("function(handle){
+        window.clearTimeout(handle);
+    }")
+    .invoke_1(handle);
 }
 
 pub fn clear_interval(handle: Handle) {
-    lazy_static::lazy_static! {
-        static ref FN: JSFunction= {
-        register_function(
-            "function(handle){
-                window.clearInterval(handle);
-            }",
-        )
-    };};
-    FN.invoke_1(handle);
+    js!("function(handle){
+        window.clearInterval(handle);
+    }")
+    .invoke_1(handle);
 }
