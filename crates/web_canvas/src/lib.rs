6f37a3456c37048dce23ea93ba5d09e5ab787727
Richard <Richard>
2020-11-27 10:27:36 -0800

    use macro

--- crates/web_canvas/src/lib.rs
+++ crates/web_canvas/src/lib.rs
@@ -8,24 +8,25 @@ pub struct CanvasContext {
 pub trait Canvas2dApi {
     fn set_fill_color(&self, color: &str);
     fn fill_rect(&self, x: impl Into<f64>, y: impl Into<f64>, w: impl Into<f64>, h: impl Into<f64>);
-    fn clear_rect(&self, x: impl Into<f64>, y: impl Into<f64>, w: impl Into<f64>, h: impl Into<f64>);
+    fn clear_rect(
+        &self,
+        x: impl Into<f64>,
+        y: impl Into<f64>,
+        w: impl Into<f64>,
+        h: impl Into<f64>,
+    );
 }
 
 impl CanvasContext {
     pub fn from_canvas_element(el: impl Into<f64>) -> CanvasContext {
         CanvasContext {
             handle: {
-                lazy_static::lazy_static! {
-                    static ref FN: JSFunction= {
-                    register_function(
-                        r#"function(el){
-                            el = this.getObject(el);
-                            let ctx = el.getContext("2d");
-                            return this.storeObject(ctx);
-                        }"#,
-                    )
-                };};
-                FN.invoke_1(el.into())
+                js!(r#"function(el){
+                    el = this.getObject(el);
+                    let ctx = el.getContext("2d");
+                    return this.storeObject(ctx);
+                }"#)
+                .invoke_1(el.into())
             },
         }
     }
@@ -33,16 +34,11 @@ impl CanvasContext {
 
 impl Canvas2dApi for CanvasContext {
     fn set_fill_color(&self, color: &str) {
-        lazy_static::lazy_static! {
-            static ref FN: JSFunction= {
-            register_function(
-                "function(ctx,strPtr,strLen){
-                                ctx = this.getObject(ctx);
-                                ctx.fillStyle = this.readUtf8FromMemory(strPtr,strLen);
-                        }",
-            )
-        };};
-        FN.invoke_3(self.handle, color.as_ptr() as u32, color.len() as u32);
+        js!("function(ctx,strPtr,strLen){
+            ctx = this.getObject(ctx);
+            ctx.fillStyle = this.readUtf8FromMemory(strPtr,strLen);
+        }")
+        .invoke_3(self.handle, color.as_ptr() as u32, color.len() as u32);
     }
 
     fn fill_rect(
@@ -52,16 +48,11 @@ impl Canvas2dApi for CanvasContext {
         w: impl Into<f64>,
         h: impl Into<f64>,
     ) {
-        lazy_static::lazy_static! {
-            static ref FN: JSFunction= {
-            register_function(
-                "function(ctx,x,y,w,h){
-                                ctx = this.getObject(ctx);
-                                ctx.fillRect(x,y,w,h);
-                        }",
-            )
-        };};
-        FN.invoke_5(self.handle, x, y, w, h);
+        js!("function(ctx,x,y,w,h){
+            ctx = this.getObject(ctx);
+            ctx.fillRect(x,y,w,h);
+        }")
+        .invoke_5(self.handle, x, y, w, h);
     }
 
     fn clear_rect(
@@ -71,15 +62,10 @@ impl Canvas2dApi for CanvasContext {
         w: impl Into<f64>,
         h: impl Into<f64>,
     ) {
-        lazy_static::lazy_static! {
-            static ref FN: JSFunction= {
-            register_function(
-                "function(ctx,x,y,w,h){
-                                ctx = this.getObject(ctx);
-                                ctx.clearRect(x,y,w,h);
-                        }",
-            )
-        };};
-        FN.invoke_5(self.handle, x, y, w, h);
+        js!("function(ctx,x,y,w,h){
+            ctx = this.getObject(ctx);
+            ctx.clearRect(x,y,w,h);
+        }")
+        .invoke_5(self.handle, x, y, w, h);
     }
 }
