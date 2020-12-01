287b5da8a326bd2364d569b34fffb5c98aa6f0fd
Richard <Richard>
2020-11-30 19:05:02 -0800

    new flat generator

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ build:
 	js-bindgen --language c bindings/web_console.yaml > headers/web_console.h
 	js-bindgen --language c bindings/web_canvas.yaml > headers/web_canvas.h
 	js-bindgen --language c bindings/web_dom.yaml > headers/web_dom.h
+	js-bindgen --language rust bindings/web_console.yaml > crates/web_console/src/lib.rs
--- crates/web_console/src/lib.rs
+++ crates/web_console/src/lib.rs
@@ -1,56 +1,55 @@
 #![no_std]
-pub mod console {
-    use js::*;
-    
-    pub fn clear(){
-        let func = js!(r###"function(){
+
+use js::*;
+
+pub fn console_clear() {
+    let func = js!(r###"function(){
                 console.clear();
-            }"###);
-        func.invoke_0();
-    }
-    
-    pub fn log(msg: &str){
-        let a0 = msg.as_ptr() as u32;
-        let a1 = msg.len() as u32;
-        let func = js!(r###"function(msgPtr,msgLen){
+        }"###);
+    func.invoke_0();
+}
+
+pub fn console_log(msg: &str) {
+    let a0 = msg.as_ptr() as u32;
+    let a1 = msg.len() as u32;
+    let func = js!(r###"function(msgPtr,msgLen){
                 console.log(this.readUtf8FromMemory(msgPtr,msgLen));
-            }"###);
-        func.invoke_2(a0, a1);
-    }
-    
-    pub fn warning(msg: &str){
-        let a0 = msg.as_ptr() as u32;
-        let a1 = msg.len() as u32;
-        let func = js!(r###"function(msgPtr,msgLen){
+        }"###);
+    func.invoke_2(a0, a1);
+}
+
+pub fn console_warning(msg: &str) {
+    let a0 = msg.as_ptr() as u32;
+    let a1 = msg.len() as u32;
+    let func = js!(r###"function(msgPtr,msgLen){
                 console.warn(this.readUtf8FromMemory(msgPtr,msgLen));
-            }"###);
-        func.invoke_2(a0, a1);
-    }
-    
-    pub fn error(msg: &str){
-        let a0 = msg.as_ptr() as u32;
-        let a1 = msg.len() as u32;
-        let func = js!(r###"function(msgPtr,msgLen){
+        }"###);
+    func.invoke_2(a0, a1);
+}
+
+pub fn console_error(msg: &str) {
+    let a0 = msg.as_ptr() as u32;
+    let a1 = msg.len() as u32;
+    let func = js!(r###"function(msgPtr,msgLen){
                 console.error(this.readUtf8FromMemory(msgPtr,msgLen));
-            }"###);
-        func.invoke_2(a0, a1);
-    }
-    
-    pub fn time(msg: &str){
-        let a0 = msg.as_ptr() as u32;
-        let a1 = msg.len() as u32;
-        let func = js!(r###"function(msgPtr,msgLen){
+        }"###);
+    func.invoke_2(a0, a1);
+}
+
+pub fn console_time(msg: &str) {
+    let a0 = msg.as_ptr() as u32;
+    let a1 = msg.len() as u32;
+    let func = js!(r###"function(msgPtr,msgLen){
                 console.time(this.readUtf8FromMemory(msgPtr,msgLen));
-            }"###);
-        func.invoke_2(a0, a1);
-    }
-    
-    pub fn time_end(msg: &str){
-        let a0 = msg.as_ptr() as u32;
-        let a1 = msg.len() as u32;
-        let func = js!(r###"function(msgPtr,msgLen){
+        }"###);
+    func.invoke_2(a0, a1);
+}
+
+pub fn console_time_end(msg: &str) {
+    let a0 = msg.as_ptr() as u32;
+    let a1 = msg.len() as u32;
+    let func = js!(r###"function(msgPtr,msgLen){
                 console.timeEnd(this.readUtf8FromMemory(msgPtr,msgLen));
-            }"###);
-        func.invoke_2(a0, a1);
-    }
+        }"###);
+    func.invoke_2(a0, a1);
 }
