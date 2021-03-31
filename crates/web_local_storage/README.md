83020e3abd81b8028d20d0a7b0bbe56f6cb05ede
Richard Anaya <richard.anaya@gmail.com>
2021-03-30 19:07:31 -0700

    first commit

---
+++ crates/web_local_storage/README.md
@@ -0,0 +1,20 @@
+# web_console
+
+<a href="https://docs.rs/web_local_storage"><img src="https://img.shields.io/badge/docs-latest-blue.svg?style=flat-square" alt="docs.rs docs" /></a>
+
+# License
+
+This project is licensed under either of
+
+ * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+   http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+   http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in 'web_local_storage' by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
---
+++ crates/web_local_storage/src/lib.rs
@@ -0,0 +1,50 @@
+#![no_std]
+extern crate alloc;
+use alloc::string::String;
+
+use js::*;
+
+pub fn local_storage_set_item(id: &str, data: &str) {
+    let a0 = id.as_ptr() as u32;
+    let a1 = id.len() as u32;
+    let d0 = data.as_ptr() as u32;
+    let d1 = data.len() as u32;
+    let func = js!(r###"function(idPtr,idLen,dataPtr,dataLen){
+                window.localStorage.setItem(this.readUtf8FromMemory(idPtr,idLen),this.readUtf8FromMemory(dataPtr,dataLen));
+        }"###);
+    func.invoke_4(a0, a1, d0, d1);
+}
+
+pub fn local_storage_get_item(id: &str) -> Option<String> {
+    let a0 = id.as_ptr() as u32;
+    let a1 = id.len() as u32;
+    let func = js!(r###"function(idPtr,idLen){
+                const a = window.localStorage.getItem(this.readUtf8FromMemory(idPtr,idLen),this.readUtf8FromMemory(dataPtr,dataLen));
+                if(a === null){
+                    return -1;
+                } 
+                return this.writeCStringToMemory(a);
+        }"###);
+    let txt = func.invoke_2(a0, a1);
+    if txt == -1.0 {
+        return None;
+    } else {
+        Some(cstr_to_string(txt as i32))
+    }
+}
+
+pub fn local_storage_remove_item(id: &str) {
+    let a0 = id.as_ptr() as u32;
+    let a1 = id.len() as u32;
+    let func = js!(r###"function(idPtr,idLen){
+                window.localStorage.removeItem(this.readUtf8FromMemory(idPtr,idLen));
+        }"###);
+    func.invoke_2(a0, a1);
+}
+
+pub fn local_storage_clear() {
+    let func = js!(r###"function(idPtr,idLen,dataPtr,dataLen){
+                window.localStorage.clear();
+        }"###);
+    func.invoke_0();
+}
