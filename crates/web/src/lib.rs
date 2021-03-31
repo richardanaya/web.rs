c9dc4fd92a96cfa89e634e92e758e6d6279433b4
Richard Anaya <richard.anaya@gmail.com>
2021-03-30 19:26:05 -0700

    adding new local storage

--- crates/web/src/lib.rs
+++ crates/web/src/lib.rs
@@ -1,12 +1,12 @@
 #![no_std]
+pub use html_color::*;
 pub use js::*;
 pub use spin::{Mutex, MutexGuard};
 pub use web_canvas::*;
 pub use web_common::*;
 pub use web_console::*;
 pub use web_dom::*;
-pub use web_random::*;
 pub use web_local_storage::*;
+pub use web_random::*;
 pub use web_timer::*;
-pub use html_color::*;
 pub use webcomponent::*;
