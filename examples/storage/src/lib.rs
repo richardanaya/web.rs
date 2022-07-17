d2bffa0f5cdbe12b83afd5e253e4012a27d391bf
Richard Anaya <richard.anaya@gmail.com>
2022-07-16 20:17:36 -0700

    updated example

--- examples/storage/src/lib.rs
+++ examples/storage/src/lib.rs
@@ -1,4 +1,5 @@
-use web::*;
+use web_console::*;
+use web_local_storage::*;
 
 #[no_mangle]
 pub fn main() {
@@ -6,25 +7,25 @@ pub fn main() {
     let a = local_storage_get_item("foo");
     match a {
         Some(s) => console_log(&s),
-        None => console_log("nothing in foo")
+        None => console_log("nothing in foo"),
     }
-    local_storage_set_item("foo","abc");
+    local_storage_set_item("foo", "abc");
     let a = local_storage_get_item("foo");
     match a {
         Some(s) => console_log(&s),
-        None => console_log("nothing in foo")
+        None => console_log("nothing in foo"),
     }
     local_storage_remove_item("foo");
     let a = local_storage_get_item("foo");
     match a {
         Some(s) => console_log(&s),
-        None => console_log("nothing in foo")
+        None => console_log("nothing in foo"),
     }
-    local_storage_set_item("foo","abc");
+    local_storage_set_item("foo", "abc");
     local_storage_clear();
     let a = local_storage_get_item("foo");
     match a {
         Some(s) => console_log(&s),
-        None => console_log("nothing in foo")
+        None => console_log("nothing in foo"),
     }
 }
