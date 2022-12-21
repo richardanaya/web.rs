60430ef9baedbe6b754fc575e62a667b2c3443cd
Richard Anaya <richard.anaya@gmail.com>
2022-12-20 22:41:30 -0800

    fix

--- crates/web/src/lib.rs
+++ crates/web/src/lib.rs
@@ -291,12 +291,12 @@ pub fn element_remove_mouse_up_listener(
 // Request Animation Frame
 
 static ANIMATION_FRAME_EVENT_HANDLERS: Mutex<
-    Option<HashMap<FunctionHandle, Box<dyn FnMut() + Send + 'static>>>,
+    Option<HashMap<i64, Box<dyn FnMut() + Send  + 'static>>>,
 > = Mutex::new(None);
 
 fn add_animation_frame_event_handler(
-    function_handle: FunctionHandle,
-    handler: Box<dyn FnMut() + Send + 'static>,
+    function_handle: i64,
+    handler: Box<dyn FnMut() + Send  + 'static>,
 ) {
     let mut h = ANIMATION_FRAME_EVENT_HANDLERS.lock().unwrap();
     if h.is_none() {
@@ -307,31 +307,34 @@ fn add_animation_frame_event_handler(
 
 #[no_mangle]
 pub extern "C" fn web_handle_animation_frame_event_handler(id: i64) {
-    let mut handlers = ANIMATION_FRAME_EVENT_HANDLERS.lock().unwrap();
-    if let Some(h) = handlers.as_mut() {
-        for (key, handler) in h.iter_mut() {
-            if key.0.value == id {
-                handler();
+    let mut c = None;
+    {
+        let mut handlers = ANIMATION_FRAME_EVENT_HANDLERS.lock().unwrap();
+        if let Some(h) = handlers.as_mut() {
+            // remove 
+            if let Some(handler) = h.remove(& id ) {
+                c = Some(handler);
             }
         }
     }
+    if let Some(mut c) = c {
+        c();
+    }
 }
 
-pub fn request_animation_frame(handler: impl FnMut() + Send + 'static) {
+pub fn request_animation_frame(handler: impl FnMut() + Send + Sync + 'static) {
     let function_ref = js!(r#"
         function(){
             const handler = () => {
                 this.module.instance.exports.web_handle_animation_frame_event_handler(id);
+                this.deleteObject(id);
             };
             const id = this.storeObject(handler);
             requestAnimationFrame(handler);
             return id;
         }"#)
     .invoke_and_return_bigint(&[]);
-    let function_handle = FunctionHandle(ExternRef {
-        value: function_ref,
-    });
-    add_animation_frame_event_handler(function_handle, Box::new(handler));
+    add_animation_frame_event_handler(function_ref, Box::new(handler));
 }
 
 // Keyboard Events
