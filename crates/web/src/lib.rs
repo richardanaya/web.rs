ff46edd147bafa8ef10757c2b7bb9901ad4fdbf1
Richard Anaya <richard.anaya@gmail.com>
2022-12-22 23:51:59 -0800

    adding history and location

--- crates/web/src/lib.rs
+++ crates/web/src/lib.rs
@@ -85,6 +85,14 @@ pub fn element_remove_class(element: &ExternRef, class: &str) {
     remove_class.invoke(&[element.into(), class.into()]);
 }
 
+pub fn element_set_style_attribute(element: &ExternRef, attribute: &str, value: &str) {
+    let set_style_attribute = js!(r#"
+        function(element, attribute, value){
+            element.style[attribute] = value;
+        }"#);
+    set_style_attribute.invoke(&[element.into(), attribute.into(), value.into()]);
+}
+
 pub fn element_set_attribute(element: &ExternRef, attribute: &str, value: &str) {
     let set_attribute = js!(r#"
         function(element, attribute, value){
@@ -1087,4 +1095,195 @@ pub fn fetch(
         let text = r2.response_text();
         callback(status, text);
     });
+}
+
+// History API
+pub fn history_push_state(title: &str, url: &str) {
+    js!("
+        function(title, url) {
+            window.history.pushState({}, title, url);
+        }
+        ")
+    .invoke(&[title.into(), url.into()]);
+}
+
+pub fn history_replace_state(title: &str, url: &str) {
+    js!("
+        function(title, url) {
+            window.history.replaceState({}, title, url);
+        }
+        ")
+    .invoke(&[title.into(), url.into()]);
+}
+
+pub fn history_back() {
+    js!("
+        function() {
+            window.history.back();
+        }
+        ")
+    .invoke(&[]);
+}
+
+pub fn history_forward() {
+    js!("
+        function() {
+            window.history.forward();
+        }
+        ")
+    .invoke(&[]);
+}
+
+pub fn history_go(delta: i32) {
+    js!("
+        function(delta) {
+            window.history.go(delta);
+        }
+        ")
+    .invoke(&[delta.into()]);
+}
+
+pub fn history_length() -> usize {
+    js!("
+        function() {
+            return window.history.length;
+        }
+        ")
+    .invoke(&[]) as usize
+}
+
+pub fn location_url() -> String {
+    js!("
+        function() {
+            return window.location.href;
+        }
+        ")
+    .invoke_and_return_string(&[])
+}
+
+pub fn location_host() -> String {
+    js!("
+        function() {
+            return window.location.host;
+        }
+        ")
+    .invoke_and_return_string(&[])
+}
+
+pub fn location_hostname() -> String {
+    js!("
+        function() {
+            return window.location.hostname;
+        }
+        ")
+    .invoke_and_return_string(&[])
+}
+
+pub fn location_pathname() -> String {
+    js!("
+        function() {
+            return window.location.pathname;
+        }
+        ")
+    .invoke_and_return_string(&[])
+}
+
+pub fn location_search() -> String {
+    js!("
+        function() {
+            return window.location.search;
+        }
+        ")
+    .invoke_and_return_string(&[])
+}
+
+pub fn location_hash() -> String {
+    js!("
+        function() {
+            return window.location.hash;
+        }
+        ")
+    .invoke_and_return_string(&[])
+}
+
+pub fn location_reload() {
+    js!("
+        function() {
+            window.location.reload();
+        }
+        ")
+    .invoke(&[]);
+}
+
+// PopState Events
+pub struct PopStateEvent {}
+
+static HISTORY_POP_STATE_EVENT_HANDLERS: Mutex<
+    Option<HashMap<Arc<FunctionHandle>, Box<dyn FnMut(PopStateEvent) + Send + 'static>>>,
+> = Mutex::new(None);
+
+fn add_history_pop_state_event_handler(
+    id: Arc<FunctionHandle>,
+    handler: Box<dyn FnMut(PopStateEvent) + Send + 'static>,
+) {
+    let mut handlers = HISTORY_POP_STATE_EVENT_HANDLERS.lock().unwrap();
+    if let Some(h) = handlers.as_mut() {
+        h.insert(id, handler);
+    } else {
+        let mut h = HashMap::new();
+        h.insert(id, handler);
+        *handlers = Some(h);
+    }
+}
+
+fn remove_history_pop_state_event_handler(id: &Arc<FunctionHandle>) {
+    let mut handlers = HISTORY_POP_STATE_EVENT_HANDLERS.lock().unwrap();
+    if let Some(h) = handlers.as_mut() {
+        h.remove(id);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn web_handle_history_pop_state_event(id: i64) {
+    let mut handlers = HISTORY_POP_STATE_EVENT_HANDLERS.lock().unwrap();
+    if let Some(h) = handlers.as_mut() {
+        for (key, handler) in h.iter_mut() {
+            if key.0.value == id {
+                handler(PopStateEvent {});
+            }
+        }
+    }
+}
+
+pub fn add_history_pop_state_event_listener(
+    element: &ExternRef,
+    handler: impl FnMut(PopStateEvent) + Send + 'static,
+) -> Arc<FunctionHandle> {
+    let function_ref = js!(r#"
+        function(element ){
+            const handler = (e) => {
+                this.module.instance.exports.web_handle_history_pop_state_event(id);
+            };
+            const id = this.storeObject(handler);
+            window.addEventListener("popstate",handler);
+            return id;
+        }"#)
+    .invoke_and_return_bigint(&[element.into()]);
+    let function_handle = Arc::new(FunctionHandle(ExternRef {
+        value: function_ref,
+    }));
+    add_history_pop_state_event_handler(function_handle.clone(), Box::new(handler));
+    function_handle
+}
+
+pub fn remove_history_pop_state_listener(
+    element: &ExternRef,
+    function_handle: &Arc<FunctionHandle>,
+) {
+    js!(r#"
+        function(element, f){
+            window.removeEventListener("popstate", f);
+        }"#)
+    .invoke(&[element.into(), (&(function_handle.0)).into()]);
+    remove_history_pop_state_event_handler(function_handle);
 }
