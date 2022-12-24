92b973780afd3ae5286afb4d00986395232b9e60
Richard Anaya <richard.anaya@gmail.com>
2022-12-24 13:51:14 -0800

    web macros

---
+++ crates/web_macros/src/lib.rs
@@ -0,0 +1,22 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input};
+
+#[proc_macro_attribute]
+pub fn main(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as syn::ItemFn);
+
+    let name = input.sig.ident;
+    let block = input.block;
+
+    let expanded = quote! {
+        pub fn #name() -> Result<(), JsValue> {
+            web::run_event_loop();
+            executor::run(async move {
+                #block
+            })
+        }
+    };
+
+    TokenStream::from(expanded)
+}
